d94ae01c17c5c76da1977d1d7658c744b7e9780b
1-hemant-singh <123456hemantsingh@gmail.com>
2025-06-19 16:12:52 +0530

    img src edit

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -10,7 +10,7 @@ function PostCard({$id, title, featuredImage}) {
     <Link to={`/post/${$id}`}>
         <div className='w-full bg-gray-100 rounded-xl p-4'>
             <div className='w-full justify-center mb-4'>
-                <img src={appwriteService.getFilePreview(featuredImage)+"&mode=admin"} alt={title} 
+                <img src={appwriteService.getFilePreview(featuredImage)} alt={title} 
                 // <img src={"https://fra.cloud.appwrite.io/v1/storage/buckets/684d20d9002280e4678d/files/685269f49077fcc8e074/view?project=684d1d83002a58f7d6f5&mode=admin"} alt={title}
                 className='rounded-xl' />
 
